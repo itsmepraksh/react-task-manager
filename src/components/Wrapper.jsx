313f0fb40d13dcbea97ec74784cf0535bebc843a
Prakash Samanta <druvils123@gmail.com>
2025-06-15 22:07:16 +0530

    Day 3 completed the localStorage part where data will store

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,9 +1,17 @@
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react"
 
 export const dataContext =  createContext(null);
 
 const Wrapper = (props) => {
-  const [task, setTask] = useState([]);
+  const [task, setTask] = useState(()=>{
+    const savedTask = localStorage.getItem("taskList")
+    return savedTask ? JSON.parse(savedTask) : []
+  })
+
+  useEffect(() => {
+    localStorage.setItem("taskList", JSON.stringify(task))
+  }, [task])
+  
     
   return (
      
